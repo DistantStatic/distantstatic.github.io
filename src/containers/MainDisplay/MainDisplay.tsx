b14b924cde194503b58078439544155a7f28d9d9
Oscar Rodriguez <oscarrodriguez101@gmail.com>
2021-11-20 02:49:37 -0500

    added initial surge animation

--- src/containers/MainDisplay/MainDisplay.tsx
+++ src/containers/MainDisplay/MainDisplay.tsx
@@ -1,10 +1,11 @@
 import Projects from '../../components/Projects/Projects';
 import PageTitle from '../../components/PageTitle/PageTitle';
+import classes from './MainDisplay.module.css';
 
 export default function MainDisplay({darkMode, changeDarkMode}: {darkMode: boolean, changeDarkMode: ()=>void}) {
 
     return (
-    <main id="main">
+    <main id="main" className={classes.MainDisplay}>
         <PageTitle changeMode={changeDarkMode}/>
         <Projects darkMode={darkMode} />
     </main>
